ce767d85a961d86461d04212b797d0a5c33d67c7
Joel Pires <joelpires70@gmail.com>
2022-09-22 12:27:36 -0300

    cases tests complete

--- maquina_turing/analytics.py
+++ maquina_turing/analytics.py
@@ -3,6 +3,7 @@ import numpy as np
 import time
 import glob
 from tm import TM
+from threading import Thread
 
 def d_from_file(file):
     """Definition from file"""
@@ -21,35 +22,47 @@ def get_definitions(path):
 
     return definitions
 
-def delta_time(ext_delta, words, name):
+def process_word(turing_reference, word, result):
+    result['t0'] = time.time()
+    turing_reference.extended_delta(word)
+    result['t1'] = time.time()
+
+def delta_time(mt_instance, words, name, time_off):
     time_df = []
     for w in words:
-        t0 = time.time()
-        ext_delta(w)
-        t1 = time.time()
 
-        time_df.append({"deltaT": t1-t0,
-                        "len_w": len(w),
-                        "name": name})
+        result = dict({"t0": time.time(),
+                        "t1": None})
+
+        t = Thread(target=process_word, args=(mt_instance, w, result))
+        t.start()
+
+        while (result['t1'] == None and (time.time() - result["t0"]) < time_off): #wait, on the main thread, if the result changes. If not change and time out of the time_off, stop this thread
+            time.sleep(0.1) #sleep for 0.1 seconds
+
+        if result['t1'] != None: #compute only the occurrences that MT not loop
+            time_df.append({"deltaT": result['t1']-result['t0'],
+                            "len_w": len(w),
+                            "name": name})
 
     return time_df
 
-def generate_words(x,y):
+def generate_words(x,y, qtd=10_000, step=100):
     words = []
 
-    for k in np.arange(0, 10_000, 100):
+    for k in np.arange(0, qtd, step):
         x_k = x*int(k/2)
         y_k = y*int(k/2)
         words.append(x_k+y_k)
         words.append(y_k+x_k)
     return words
 
-def case_test(path, name, x, y):
+def case_test(path, name, x, y, qtd, step, time_off):
     mt_def = d_from_file(path+"/"+name+".tm")
 
     mt_instance = TM()
     mt_instance.setup(mt_def)
 
     ## Generate too many words
-    words = generate_words(x=x,y=y)
-    pd.DataFrame(delta_time(mt_instance.extended_delta, words, name)).to_csv(path+"/test_cases/"+name+".csv", index=False)
+    words = generate_words(x=x,y=y, qtd = qtd, step = step)
+    pd.DataFrame(delta_time(mt_instance, words, name, time_off)).to_csv(path+"/test_cases/"+name+".csv", index=False)
--- maquina_turing/multiprocess_tests.py
+++ maquina_turing/multiprocess_tests.py
@@ -1,28 +1,38 @@
 from multiprocessing import Process
 from analytics import *
+from threading import Thread
+
+def new_thread(path, name, x, y, qtd, step, time_off):
+    p1 = Process(target=case_test, args=(path, name, x, y, qtd, step, time_off))
+    p1.start()
+    p1.join() #wait for the finish
+    print(name+" finished")
 
 if __name__ == "__main__":
+
+    """
     #Maquina de Turing 1:
     print("M1")
-    p1 = Process(target=case_test, args=("./data", "def1", "a", "b"))
-    p1.start()
+    t1 = Thread(target=new_thread, args=("./data", "def1", "a", "b", 1_000, 100, 30))
+    t1.start()
 
     #Maquina de Turing 2:
     print("M2")
-    p2 = Process(target=case_test, args=("./data", "def2", "0", "1"))
-    p2.start()
-
+    t2 = Thread(target=new_thread, args=("./data", "def2", "0", "1", 10_000, 100, 30))
+    t2.start()
+    """
     #Maquina de Turing 3:
     print("M3")
-    p3 = Process(target=case_test, args=("./data", "def3", "a", "a"))
-    p3.start()
+    t3 = Thread(target=new_thread, args=("./data", "def3", "a", "a",  2_000, 100, 30))
+    t3.start()
 
+    """
     #Maquina de Turing 4:
     print("M4")
-    p4 = Process(target=case_test, args=("./data", "def4", "a", "a"))
-    p4.start()
-
+    t4 = Thread(target=new_thread, args=("./data", "def4", "a", "a", 10_000, 100, 30))
+    t4.start()
+    """
     #Maquina de Turing 5:
     print("M5")
-    p5 = Process(target=case_test, args=("./data", "def5", "1", "1"))
-    p5.start()
+    t5 = Thread(target=new_thread, args=("./data", "def5", "1", "1",  2_000, 100, 30))
+    t5.start()
